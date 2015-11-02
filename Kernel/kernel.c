5c5c16c0cc41bed9413363f8c210ddb13d9ad6df
Pablo Alejandro Costesich <rhfixer@gmail.com>
2015-11-02 12:33:08 -0300

    fix build

--- Kernel/kernel.c
+++ Kernel/kernel.c
@@ -16,12 +16,9 @@ extern uint8_t endOfKernelBinary;
 extern uint8_t endOfKernel;
 
 static const uint64_t PageSize = 0x4000;
+static const void * shellModuleAddress = (void*)0x400000;
 
 typedef int (*EntryPoint)(unsigned int pcount, char * pgname[], void * pgptrs[]);
-
-static const EntryPoint shellModuleAddress = (EntryPoint) 0x400000;
-
-unsigned int timer = 0;
 
 void clearBSS(void * bssAddress, uint64_t bssSize)
 {
@@ -43,7 +40,7 @@ void * initializeKernelBinary()
 	 * IT BREAKS, LIKE, *REALLY* BAD.
 	 */
 	void * moduleAddresses[] = {
-	    (void *)(EntryPoint *) &shellModuleAddress
+	    (void *) shellModuleAddress
 	};
 
 	loadModules(&endOfKernelBinary, moduleAddresses);
@@ -54,7 +51,6 @@ void * initializeKernelBinary()
 void wake_up(void)
 {
 	syscall_wake();
-	timer = 0;
 }
 
 void pit_irq(int irq)
@@ -70,18 +66,20 @@ void kbrd_irq_with_activity(int irq)
 
 int main()
 {	
-	/* driver initialization */
-	/* set up IDTs & int80h */
+	EntryPoint init = (EntryPoint) *((EntryPoint *) &shellModuleAddress);
 
+	/* set up IDTs & int80h */
 	install_syscall_handler((IntSysHandler) &int80h);
 	install_hw_handler((IntHwHandler) &kbrd_irq_with_activity, INT_KEYB);
 	install_hw_handler((IntHwHandler) &pit_irq, INT_PIT);
 	install_interrupts();
 
+	/* driver initialization */
 	kbrd_install();
 	vid_clr();
 
-    (shellModuleAddress)(0, (char **) 0, (void *) 0);
+	/* Drop to environment */
+    ((EntryPoint) init)(0, (char **) 0, (void *) 0);
 
     syscall_halt();
 	return 0;
--- Kernel/mmu.c
+++ Kernel/mmu.c
@@ -1,1 +1,4 @@
 #include <mmu.h>
+#include <stddef.h>
+void * mmu_page_alloc(size_t size);
+void * mmu_page_free(void * ptr);
