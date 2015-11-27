71316d3ae9902057acff92d351878f9dbfbe99a7
Pablo Alejandro Costesich <pcostesich@monits.com>
2015-11-27 17:32:55 -0300

    Blue Sun MOTD (Browncoats ftw)

---
+++ Kernel/motd.c
@@ -0,0 +1,30 @@
+#include <motd.h>
+#include <stdio.h>
+
+void motd(void)
+{
+	puts("\n");
+ 	puts("     ++++++/////++++/:` `+++++           .++++++`     /+++++- `++++////// \n");
+ 	puts("     mmmmmd`    +mmmmmy .mmmmd`          -mmmmmd`     hmmmmm/ .dmmm:      \n");
+ 	puts("     mmmmmd     +mmmmmy .mmmmd`          -mmmmmd`     hmmmmm/ .dmmm:      \n");
+ 	puts("     mmmmmd-----ommmdy/ .mmmmd`          -mmmmmd`     hmmmmm/ .dmmm+::::: \n");
+ 	puts("     mmmmmd`````+mmmmdo .mmmmd`          -mmmmmd`     hmmmmm/ .dmmm/````` \n");
+ 	puts("     mmmmmd     +mmmmmy .mmmmd`          -mmmmmd`     hmmmmm/ .dmmm:      \n");
+ 	puts("     mmmmmd-....ommmmds .mmmmd.......... -dmmmmd......hmmmmm/ .dmmm+..... \n");
+ 	puts("     mdhhhhhhhhhhhhhhyssymmmdhhhhhhhddddyyhhhhhhhhddhhhhhhhhhyhmmmmdhhhhh \n");
+ 	puts("     -`````        `````./mm:``````+mmmmmm+``````:md````````+dmmmmms````` \n");
+ 	puts("           .mmmmmms      -mm:      +mmmmmm+      -md         .odmmms      \n");
+ 	puts("            mmmmmmhssssssymm:      +mmmmmm+      -md     :-    -smms      \n");
+ 	puts("     o/::            ..-:smm:      +mmmmmm+      -md     +do.    :ys      \n");
+ 	puts("     ++++++ommmmmms      -mm:      +mmmmmm+      -md     +mmh/`   `-      \n");
+ 	puts("           .mmmmmms      -mm:      +mmmmmm+      -md     +mmmmh:          \n");
+ 	puts("     /-..           ..-:ommo:-...            ..-:omd.....ommmmmds-....... \n");
+ 	puts("     +hdddddddddddddddddmmmmmmddddddddddddddddddmmmmddddddmmmmmmmddddddy- \n");
+ 	puts("      .odmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmh+`  \n");
+ 	puts("        .+hdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdy/`    \n");
+ 	puts("          `-ohdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdy+.       \n");
+ 	puts("             `-/shdmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdyo:.          \n");
+ 	puts("                 `.:+syhddmmmmmmmmmmmmmmmmmmmmmmmmmmddhyo/-.`             \n");
+ 	puts("                       `.-:+osyyyhhhhdddhhhhhyysoo/:-.`   				   \n");
+ 	puts("\n");	
+}
